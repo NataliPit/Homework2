8bff4ef31673478e1cacdb89e6e1483d2ca9c8b5
Natali <saromydova-natasha@yandex.ru>
2023-06-17 11:55:44 +0300

    Homework2 C# Task3

---
+++ Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int ReadInt(string message)
+{
+    System.Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int number = ReadInt("Введите число: ");
+if (number >= 100)
+{
+    while (number > 1000)
+    {
+        number = number / 10;
+    }
+    number = number % 10;
+    System.Console.Write($"Третья цифра в числе {number} - {number}");
+}
+else
+{
+System.Console.WriteLine("Число должно состоять из трех чисел или больше!");    
+}
+
