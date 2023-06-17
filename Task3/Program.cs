54556c7508054812084a9eb089ccfc1168da2cc4
Natali <saromydova-natasha@yandex.ru>
2023-06-17 12:27:00 +0300

    Homework2 C# Task 3.0

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -10,25 +10,18 @@ int ReadInt(string message)
     return value;
 }
 
-bool validate7sign(int number)
-{
-    if (number >= 1 && number <= 7)
-    {
-        return true;
-    }
 
-    System.Console.WriteLine("Число должно быть в диапазоне от 1 до 7!");
-    return false;
-}
-
-int number = ReadInt("Введите число ");
-if (number >= 1 && number <= 5)
+int number = ReadInt("Введите число от 1 до 7: ");
+if (number >= 1 && number <= 7)
 {
-    int Digit = number;
+    if (number >=1 && number <=5)
     System.Console.Write($"Сегодня {number} день недели - сегодня будний день.");
-}
-else
-{
-    int Digit = number;
+    else
     System.Console.Write($"Сегодня {number} день недели - сегодня выходной!");
 }
+
+else
+{
+    System.Console.WriteLine("Число должно быть в диапазоне от 1 до 7!");
+}
+
